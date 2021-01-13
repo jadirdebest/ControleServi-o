3f8b0d0cebacb1498419b4e944aab0f7bb9db0dc
Jadir Junior da Silva Alves <jadir.alves@foa.org.br>
2021-01-13 13:40:38 -0300

    Adicionar arquivos de projeto.

---
+++ ControleServiço.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ControleServiço.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ ControleServiço/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ControleServiço.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
